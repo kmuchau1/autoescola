a1e940f0f536da883912d726883356d59d14658c
Karine Muchau Moraes <karine.moraes@lactec.org.br>
2021-11-28 11:07:36 -0300

    Adicionado Accordion componentes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,6 +10,7 @@ import Feature from "components/atoms/Feature";
 import Section from "components/molecules/Section";
 import Footer from "components/organisms/Footer";
 import ProductGrid from "components/organisms/ProductGrid";
+import Accordion, { AccordionGroup } from "components/atoms/Accordion";
 
 import HeroImage from "assets/hero.jpg";
 import ServImage01 from "assets/serv-01.jpg";
@@ -118,13 +119,29 @@ const Home = () => (
       <Heading>
         <h2>Dúvidas recorrentes</h2>
       </Heading>
+      <AccordionGroup>
+        <Accordion title="Como eu renovo minha CNH?">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam
+          veritatis autem reprehenderit necessitatibus laborum perspiciatis.
+          Repellat mollitia minus, eum eos excepturi quos placeat culpa ea
+          consequuntur facere vel. Consequatur, reiciendis.
+        </Accordion>
+        <Accordion title="Como faço a mudança de categoria?">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam
+          veritatis autem reprehenderit necessitatibus laborum perspiciatis.
+          Repellat mollitia minus, eum eos excepturi quos placeat culpa ea
+          consequuntur facere vel. Consequatur, reiciendis.
+        </Accordion>
+        <Accordion title="Fui multado, e agora?">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam
+          veritatis autem reprehenderit necessitatibus laborum perspiciatis.
+          Repellat mollitia minus, eum eos excepturi quos placeat culpa ea
+          consequuntur facere vel. Consequatur, reiciendis.
+        </Accordion>
+      </AccordionGroup>
     </Section>
     <Footer />
   </>
 );
-
-// Home.defaultProps = {};
-
-// Home.propTypes = {};
 
 export default Home;
