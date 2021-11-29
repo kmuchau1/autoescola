a0482e8cb43b8e8df4333205741cba127f52e2a8
Karine Muchau Moraes <karine.moraes@lactec.org.br>
2021-11-29 11:28:45 -0300

    Correções home

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: [
-    "../src/stories/**/*.stories.(jsx|mdx)",
-    "../src/components/**/*.stories.(jsx|mdx)",
-    "../src/styles/**/*.stories.(jsx|mdx)",
+    "../src/stories/**/*.stories.@(jsx|mdx)",
+    "../src/components/**/*.stories.@(jsx|mdx)",
+    "../src/styles/**/*.stories.@(jsx|mdx)",
   ],
   addons: [
     "@storybook/addon-links",
